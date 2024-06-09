3dee518e131b37fead892a190e2d88703db2278d
op2319 <omkar.patil2319@gmail.com>
2024-06-09 19:54:29 -0400

    Adding Import Test.py

---
+++ import test.py
@@ -0,0 +1,17 @@
+import time
+
+print("Starting Import Processes...")
+
+start = time.time()
+
+import os
+import numpy as np 
+import scipy as sp 
+import pandas as pd 
+import matplotlib.pyplot  
+
+end = time.time()
+print("Done!")
+print(end - start, "Sec")
+
+os.system('pause')
